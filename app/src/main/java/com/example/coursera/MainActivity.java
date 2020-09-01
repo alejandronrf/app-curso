805e4ff13c6cde479a9948a6a322742dcd9da4f8
alejandronrf <alejandronrf@gmail.com>
2020-09-01 03:40:10 -0300

    Cambios Finales Entrega 1 Lista

--- app/src/main/java/com/example/coursera/MainActivity.java
+++ app/src/main/java/com/example/coursera/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.coursera;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+    Resources resources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView1);
+        button = findViewById(R.id.button);
+        resources = button.getResources();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (resources.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                    Toast.makeText(MainActivity.this, "Estamos en modo Portrait",
+                            Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Estamos en modo Landscape",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
